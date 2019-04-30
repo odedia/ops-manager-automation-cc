88a1dfb429a759da373f99d49c6929426074198a
odedia <odedia@gmail.com>
2019-04-30 12:31:48 +0300

    Update certbot.sh

--- bin/certbot.sh
+++ bin/certbot.sh
@@ -2,7 +2,7 @@
 sudo certbot --agree-tos -m ${EMAIL} \
   certonly --cert-name  ${DOMAIN}  \
   --dns-google \
-  -d "${DOMAIN}, *.${DOMAIN}, *.apps.${DOMAIN}, *.sys.${DOMAIN}, *.login.sys.${DOMAIN}, *.uaa.sys.${DOMAIN}, *.pks.${DOMAIN}" \
+  -d "${DOMAIN}, *.${DOMAIN}, *.apps.${DOMAIN}, *.sys.${DOMAIN}, *.ws.${DOMAIN}, *.login.sys.${DOMAIN}, *.uaa.sys.${DOMAIN}, *.pks.${DOMAIN}" \
   --dns-google-credentials /home/ubuntu/gcp_credentials.json \
   --dns-google-propagation-seconds 60
 
